3c01bbff0c64d50ed41aa7d0e983303b8b419003
Raunak Sett <sett.raunak@gmail.com>
2017-04-16 15:39:16 +0530

    Added Complex Class p2

--- java/p2/Arithmetic/ArithmeticDemo.java
+++ java/p2/Arithmetic/ArithmeticDemo.java
@@ -39,11 +39,38 @@ class Rational {
 }
 
 class Complex {
+    private int real;
+    private int imaginary;
 
+    Complex(int real, int imaginary){
+      this.real = real;
+      this.imaginary = imaginary;
+    }
+
+    public void add(Complex n2){
+      this.real = this.real + n2.real;
+      this.imaginary = this.imaginary + n2.imaginary;
+    }
+
+    public void sub(Complex n2){
+      this.real = this.real - n2.real;
+      this.imaginary = this.imaginary - n2.imaginary;
+    }
+
+    public void multiply(Complex n2){
+      this.real = this.real * n2.real - this.imaginary * n2.imaginary;
+      this.imaginary = this.real * n2.imaginary + this.imaginary * n2.real;
+    }
+
+    // Overriding toString Method
+    public String toString(){
+      return this.real + " + " + this.imaginary + "i";
+    }
 }
 
 class ArithmeticDemo {
     public static void main(String[] args){
+          // Showing Rational Numbers
           Rational r1 = new Rational(2,3);
           Rational r2 = new Rational(3,4);
 
@@ -57,6 +84,22 @@ class ArithmeticDemo {
 
           // Mul
           r1.multiply(r2);
-          System.out.println(r2);  
+          System.out.println(r2);
+
+          // Showing Complex Numbers
+          Complex c1 = new Complex(1,2);
+          Complex c2 = new Complex(2,3);
+
+          // Add
+          c1.add(c2);
+          System.out.println(c1);
+
+          // Subtract
+          c1.sub(c2);
+          System.out.println(c1);
+
+          // Multiply
+          c1.multiply(c2);
+          System.out.println(c1);  
     }
 }
